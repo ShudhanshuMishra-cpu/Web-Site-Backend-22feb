8449ad95909f420cd6ad6de1a0d1e8fa2a4b7bc8
ShudhanshuMishra-cpu <theshudhanshumishra@gmail.com>
2024-02-22 18:20:13 +0530

    new web site database added

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const Blog = require("./models/blogModal");
+app.use(cors());
+app.use(bodyParser.json());
+mongoose.connect(
+  "mongodb+srv://avyaanmanagementindia:JAUMuI0885QNGowH@cluster0.1mmz1qs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+app.get("/getBlogs", async (req, res) => {
+  const blogs = await Blog.find();
+  res.json(blogs);
+});
+
+app.post("/postAvyaan123", async (req, res) => {
+  const blog = new Blog({
+    title: req.body.title,
+    description: req.body.description,
+    imgUrl: req.body.imgUrl,
+  });
+  await blog.save();
+  res.json(blog);
+});
+
+app.get("/getBlog/:id", async (req, res) => {
+  const id = req.params.id;
+  const blog = await Blog.findById(id);
+  res.json(blog);
+});
+
+// rishabh2560
+// XJUHAmrOAzVUrvJD;
+
+const port = 5000;
+app.listen(port);
